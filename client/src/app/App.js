2bc0d7e555ace4c4e596f9393fef223020dd099e
Makdoom Shaikh <makshaikh99@gmail.com>
2020-12-09 21:45:55 +0530

    Implementing React-router

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,10 +1,20 @@
+import Signup from "../components/signup/Signup";
+import Login from "../components/login/Login";
+import Dashboard from "../components/dashboard/Dashboard";
+import {BrowserRouter as Router} from 'react-router-dom'
 import "./App.css";
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <h1>Password manager</h1>
+      <Signup />
+      <Login />
+      <Dashboard />
     </div>
+
+    </Router>
   );
 }
 
---
+++ client/src/components/signup/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="signup">
+      <h1>Signup</h1>
+    </div>
+  );
+};
+
+export default Signup;
