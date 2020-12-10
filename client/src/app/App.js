ae6a4ad59e57ddeae65c10745394a448b60593ea
Makdoom Shaikh <makshaikh99@gmail.com>
2020-12-10 11:25:17 +0530

    Creating UI of login component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,19 +1,19 @@
 import Signup from "../components/signup/Signup";
 import Login from "../components/login/Login";
 import Dashboard from "../components/dashboard/Dashboard";
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className="app">
-      <h1>Password manager</h1>
-      <Signup />
-      <Login />
-      <Dashboard />
-    </div>
-
+      <Switch>
+        <div className="app">
+          <Route exact path="/dash" component={Dashboard} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </div>
+      </Switch>
     </Router>
   );
 }
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 const Login = () => {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <div className="login__box">
+        <h2>Login</h2>
+        <form className="form__control">
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Don't have an account ?{" "}
+          <Link to="/signup" className="sign">
+            Signup
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
