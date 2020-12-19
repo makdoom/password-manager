58e7843ab0463f5e5b1884b7b442c8bf5d07e474
Makdoom Shaikh <makshaikh99@gmail.com>
2020-12-19 21:01:51 +0530

    modal input issue Fixed

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -20,6 +20,7 @@ const Modal = ({ modal, setModal }) => {
     try {
       const response = await axios.post("/add", newPassword);
       setModal(!modal);
+      setNewPassword({ title: "", username: "", password: "" });
       console.log(response);
     } catch (error) {
       const err = error.response.data;
@@ -27,11 +28,16 @@ const Modal = ({ modal, setModal }) => {
     }
   };
 
+  const closeModal = () => {
+    setModal(!modal);
+    setNewPassword({ title: "", username: "", password: "" });
+  };
+
   return (
     <div className="modal__container">
       <div className="modal__header">
         <h3>Add More Passwords</h3>
-        <i className="fas fa-times" onClick={() => setModal(!modal)}></i>
+        <i className="fas fa-times" onClick={closeModal}></i>
       </div>
       <div className="modal__body">
         <form className="form__control" onSubmit={handleSubmit}>
@@ -40,12 +46,14 @@ const Modal = ({ modal, setModal }) => {
             className="modal__input"
             type="text"
             placeholder="Title"
+            value={newPassword.title}
             onChange={handleChange}
           />
           <input
             name="username"
             className="modal__input"
             type="text"
+            value={newPassword.username}
             placeholder="Username"
             onChange={handleChange}
           />
@@ -53,6 +61,7 @@ const Modal = ({ modal, setModal }) => {
             name="password"
             className="modal__input"
             type="password"
+            value={newPassword.password}
             placeholder="Password"
             onChange={handleChange}
           />
